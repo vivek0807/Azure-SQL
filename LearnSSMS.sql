bea173a26fa9102dd64854c97c114abad2524229
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2021-07-02 16:07:42 +0530

    chfg

--- LearnSSMS.sql
+++ LearnSSMS.sql
@@ -7,3 +7,6 @@ select * from Production.Product order by ProductID;
 
 
 select count(*) from INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' and TABLE_SCHEMA='Person'
+
+select * from Production.Product;
+select count(*) from Production.Product;
