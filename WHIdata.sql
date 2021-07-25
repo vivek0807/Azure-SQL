9ad3f23f0d5c6e6a178203bda5dd17c86dbeedb0
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2021-07-25 16:35:33 +0530

    Added

--- WHIdata.sql
+++ WHIdata.sql
@@ -47,10 +47,6 @@ select top(100)* from dbo.whi_raw_transaction_atstate;
 select top(100)* from staging.vw_fact_orders ;
 select top(100)* from dbo.vw_fact_orders where order_dt !='2021-07-09';
 
-delete po from dbo.vw_fact_orders po
-    inner join staging.vw_fact_orders so
-    on po.order_dt=so.order_dt
-
 
 truncate table dbo.vw_dim_store
 truncate table dbo.vw_dim_product
@@ -58,3 +54,35 @@ truncate table dbo.vw_dim_product
 truncate table staging.whi_raw_transaction_atstate;
 
 delete from staging.vw_fact_orders where order_dt='2021-07-09'
+
+--------------------adding date--------------------
+truncate table staging.whi_raw_transaction_atstate;
+truncate table dbo.whi_raw_transaction_atstate;
+select top(100)* from  dbo.whi_raw_transaction_atstate;
+
+select count(*) from staging.whi_raw_transaction_atstate; --30204580
+select count(*) from dbo.whi_raw_transaction_atstate; --30204580
+
+alter table dbo.whi_raw_transaction_atstate
+    add loadtime datetime default current_timestamp;
+------------------------------------------------------
+alter table dbo. whi_raw_transaction_atzip
+add loadtime datetime default current_timestamp;
+
+select  top(100)* from  dbo.whi_raw_transaction_atzip
+select  top(100)* from  staging.whi_raw_transaction_atzip
+select count(*) from dbo.whi_raw_transaction_atzip;
+truncate table dbo.whi_raw_transaction_atzip;
+truncate table staging.whi_raw_transaction_atzip;
+
+-------------------------------------------------------
+alter table dbo.whi_data
+    add loadtime datetime default current_timestamp
+
+delete from dbo.whi_data  where dbo.whi_data.rowid in
+(select distinct sw.rowid from staging.whi_data sw)
+
+select top(100)* from dbo.whi_data;
+
+delete from dbo.vw_fact_orders where dbo.vw_fact_orders.order_dt in
+(select distinct so.order_dt from staging.vw_fact_orders so)
