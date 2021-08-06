7b3c862572f98b9019aa66a78aaeef405b94de34
vivek0807 <35535866+vivek0807@users.noreply.github.com>
2021-08-06 21:23:56 +0530

    Update CVN-pipelines.sql

--- CVN-pipelines.sql
+++ CVN-pipelines.sql
@@ -8,3 +8,4 @@ select * from staging.person where person_update_dt_tm in
 (select max(person_update_dt_tm) from staging.person group by person_id)
 
 
+select * from sys.columns where name like '%domain%'
